5e3fa3f8a1055393115374bb68247ca7ab612920
Sk Tamim <sktamim225@gmail.com>
2021-12-07 20:12:59 +0530

    Made file structure and completed Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "../assets/css/header.css";
+import logo from "../assets/images/logo.svg";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Header() {
+	return (
+		<>
+			<div className='header mx-3 px-5 py-2 d-none d-lg-flex justify-content-between align-items-center'>
+				<a href='#' className='logo'>
+					<img src={logo} alt='Logo' className='img-fluid' />
+				</a>
+
+				<div className='searchBox'>
+					<form>
+						<div className='input-group'>
+							<span class='input-group-text'>
+								<SearchIcon />
+							</span>
+							<input
+								type='search'
+								name='search'
+								id='search'
+								className='form-control'
+								placeholder='Search for your favorite groups in ATG'
+							/>
+						</div>
+					</form>
+				</div>
+
+				<div className='createAccount dropdown'>
+					<div
+						className='dropdown-toggle cursor-pointer'
+						id='createAcDropdown'
+						data-bs-toggle='dropdown'
+						aria-expanded='false'
+					>
+						Create account. <span className='text-primary'>It's free!</span>
+					</div>
+
+					<ul class='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+						<li>
+							<a class='dropdown-item' href='#'>
+								Sign Up
+							</a>
+						</li>
+						<li>
+							<a class='dropdown-item' href='#'>
+								Sign In
+							</a>
+						</li>
+						<li>
+							<a class='dropdown-item' href='#'>
+								Terms and Conditions
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Header;
